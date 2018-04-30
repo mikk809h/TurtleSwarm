ce8642a243c8089ab80722472b3ca8ff5c9b3d5b
Mikkel Andersen <3685424+mikk809h@users.noreply.github.com>
2018-04-30 21:05:34 +0200

    Added proper indentation based on CC's linting

--- slave.lua
+++ slave.lua
@@ -1,33 +1,33 @@
-term.clear()
-term.setCursorPos(1,1)
-print("Starting...")
-local width, height = term.getSize()
+term.clear( )
+term.setCursorPos( 1, 1 )
+print( "Starting..." )
+local width, height = term.getSize( )
 -- Get all peripherals
 local Modem = nil
-sleep(.05)
+sleep( .05 )
 
-for _,v in pairs(peripheral.getNames()) do
-    local tPeripheral = peripheral.getType(v)
+for _,v in pairs( peripheral.getNames( ) ) do
+    local tPeripheral = peripheral.getType( v )
     if tPeripheral == "turtle" or tPeripheral == "computer" then
-        peripheral.call(v, "turnOn")
+        peripheral.call( v, "turnOn" )
     elseif tPeripheral == "modem" then
-        Modem = peripheral.wrap(v)
+        Modem = peripheral.wrap( v )
     end
 end
 
 local function SelectEmpty()
     for i = 1, 16 do
-        if turtle.getItemCount(i) == 0 then
-            turtle.select(i)
+        if turtle.getItemCount( i ) == 0 then
+            turtle.select( i )
             return true
         end
     end
     for i = 1, 16 do
-        if turtle.getItemCount(i) > 0 then
-            local e = turtle.getItemDetail(i)
+        if turtle.getItemCount( i ) > 0 then
+            local e = turtle.getItemDetail( i )
             if e.name == "minecraft:cobblestone" or e.name == "minecraft:stone" or e.name == "minecraft:endstone" then
-                turtle.select(i)
-                turtle.drop()
+                turtle.select( i )
+                turtle.drop( )
                 return true
             end
         end
@@ -37,10 +37,10 @@ end
 
 local function FindModem()
     for i = 1, 16 do
-        if turtle.getItemCount(i) > 0 then
-            local e = turtle.getItemDetail(i)
+        if turtle.getItemCount( i ) > 0 then
+            local e = turtle.getItemDetail( i )
             if e.name == "computercraft:peripheral" or e.name == "computercraft:modem" then
-                turtle.select(i)
+                turtle.select( i )
                 turtle.equipRight()
             end
         end
@@ -49,22 +49,22 @@ end
 
 -- Validate modem
 if not Modem then
-    print("Retrying modem...")
-    FindModem()
-    Modem = peripheral.wrap("right")
+    print( "Retrying modem..." )
+    FindModem( )
+    Modem = peripheral.wrap( "right" )
     while not Modem do
-        print("Retrying modem...")
-        SelectEmpty()
-        turtle.equipRight()
-        sleep(.5)
-        turtle.equipRight()
-        sleep(1)
-        Modem = peripheral.wrap("right")
+        print( "Retrying modem..." )
+        SelectEmpty( )
+        turtle.equipRight( )
+        sleep( .5 )
+        turtle.equipRight( )
+        sleep( 1 )
+        Modem = peripheral.wrap( "right" )
     end
 end
-print("Modem initiated")
+print( "Modem initiated" )
 
-Modem.open(5261)
+Modem.open( 5261 )
 
 local Queue = {}
 --[[
@@ -81,9 +81,9 @@ local function QueueHandler()
     while true do
         table.sort( Queue, function( compare1, compare2 ) return compare1.Priority > compare2.Priority end )
         if #Queue > 0 then
-            local r = Queue[1].Thread
+            local r = Queue[ 1 ].Thread
             if r then
-                if tFilters[r] == nil or tFilters[r] == eventData[1] or eventData[1] == "terminate" then
+                if tFilters[ r ] == nil or tFilters[ r ] == eventData[ 1 ] or eventData[ 1 ] == "terminate" then
                     local ok, param = coroutine.resume( r, table.unpack( eventData, 1, eventData.n ) )
                     if not ok then
                         error( param, 0 )
@@ -100,11 +100,11 @@ local function QueueHandler()
             end
         end
         eventData = table.pack( os.pullEventRaw() )
-        local ox, oy = term.getCursorPos()
-        term.setCursorPos(1,height-1)
-        term.clearLine()
-        term.write(tostring(table.concat(eventData, ", ")))
-        term.setCursorPos(ox, oy)
+        local ox, oy = term.getCursorPos( )
+        term.setCursorPos( 1, height - 1 )
+        term.clearLine( )
+        term.write( tostring( table.concat( table.unpack( eventData ), ", " ) ) )
+        term.setCursorPos( ox, oy )
     end
 end
 
@@ -112,13 +112,13 @@ local function Listener()
     while true do
         local Event, ID, Channel, ReplyChannel, Run, Distance = os.pullEvent()
         if Event == "modem_message" then
-            if type(Run) == "table" then
-                if type(Run.Action) == "string" and type(Run.Priority) == "number" then
-                    table.insert(Queue, {
+            if type( Run ) == "table" then
+                if type( Run.Action ) == "string" and type( Run.Priority ) == "number" then
+                    table.insert( Queue, {
                         Priority = Run.Priority,
-                        Thread = coroutine.create(Run.Action),
-                    })
-                    os.queueEvent("BEGIN_THREAD")
+                        Thread = coroutine.create( Run.Action ),
+                    } )
+                    os.queueEvent( "BEGIN_THREAD" )
                 end
             end
         end
@@ -127,12 +127,12 @@ end
 
 local function SelfHandler()
     while true do
-        local FuelLevel = turtle.getFuelLevel()
-        os.setComputerLabel("BoofySloofy|" .. tostring(FuelLevel))
-        sleep(2)
+        local FuelLevel = turtle.getFuelLevel( )
+        os.setComputerLabel( "BoofySloofy|" .. tostring( FuelLevel ) )
+        sleep( 2 )
     end
 end
 
 
 
-parallel.waitForAll(QueueHandler, Listener, SelfHandler)
+parallel.waitForAll( QueueHandler, Listener, SelfHandler )
